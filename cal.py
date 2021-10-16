1c01728c9d617777e0b34e215694df37154559e7
Tuan Trinh <caovuhoangnam@outlook.com.vn>
2021-10-16 23:39:48 +0700

    update cal.py success rate

--- cal.py
+++ cal.py
@@ -8,7 +8,9 @@ if __name__ == "__main__":
     for file_name_raw in file_names:
         file_name = os.path.join("stat_saved", file_name_raw + ".csv")
         data = pd.read_csv(file_name)
+        count_all = data.shape[0]
         data = data.loc[data["Solution"] != "Not Found"]
+        count_remain = data.shape[0]
         print(file_name_raw)
         print(data.mean(axis=0))
-        print()
+        print("Success rate:", count_remain/count_all)
