9658b02ec4173f14dad134dc77d99cc457097cc0
Tuan Trinh <caovuhoangnam@outlook.com.vn>
2021-10-16 23:31:28 +0700

    Add gbs-mini_cosmos.csv and cal.py

---
+++ cal.py
@@ -0,0 +1,14 @@
+# Calculate average values of csv files
+import pandas as pd
+import os
+
+if __name__ == "__main__":
+    file_names = ("all_bfs-mini_cosmos", "all_bfs-micro_cosmos", "all_gbs-mini_cosmos", "all_gbs-micro_cosmos")
+    
+    for file_name_raw in file_names:
+        file_name = os.path.join("stat_saved", file_name_raw + ".csv")
+        data = pd.read_csv(file_name)
+        data = data.loc[data["Solution"] != "Not Found"]
+        print(file_name_raw)
+        print(data.mean(axis=0))
+        print()
